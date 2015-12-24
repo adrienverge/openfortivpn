a64bc64d93a1adaa1f4d612fbcc94e51e66174c1
Kevin McClellan <kdmcclel@gmail.com>
2015-12-24 09:30:27 -0500

    Config: add set-dns and set-routes flag to config file

--- src/config.c
+++ src/config.c
@@ -18,6 +18,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "config.h"
 #include "log.h"
@@ -45,6 +47,31 @@ int add_trusted_cert(struct vpn_config *cfg, const char *digest)
 	}
 
 	return 0;
+}
+
+/*
+ * Converts string to bool int
+ *
+ * @params[in] str  the string to read from
+ * @return          0 or 1 if successful, < 0 if unrecognized value
+ */
+static int strtob(const char* str)
+{
+	if (str[0] == '\0') {
+		return 0;
+	} else if (strcasecmp(str, "true") == 0) {
+		return 1;
+	} else if (strcasecmp(str, "false") == 0) {
+		return 0;
+	} else if (isdigit(str[0]) == 0) {
+		return -1;
+	}
+
+	long int i = strtol(str, NULL, 0);
+	if (i < 0 || i > 1) {
+		return -1;
+	}
+	return i;
 }
 
 /*
@@ -143,6 +170,22 @@ int load_config(struct vpn_config *cfg, const char *filename)
 		} else if (strcmp(key, "password") == 0) {
 			strncpy(cfg->password, val, FIELD_SIZE - 1);
 			cfg->password[FIELD_SIZE] = '\0';
+		} else if (strcmp(key, "set-dns") == 0) {
+			int set_dns = strtob(val);
+			if (set_dns < 0) {
+				log_warn("Bad set-dns in config file: \"%s\".\n",
+				         val);
+				continue;
+			}
+			cfg->set_dns = set_dns;
+		} else if (strcmp(key, "set-routes") == 0) {
+			int set_routes = strtob(val);
+			if (set_routes < 0) {
+				log_warn("Bad set-routes in config file: \"%s\".\n",
+				         val);
+				continue;
+			}
+			cfg->set_routes = set_routes;
 		} else if (strcmp(key, "trusted-cert") == 0) {
 			if (strlen(val) != SHA256STRLEN - 1) {
 				log_warn("Bad certificate sha256 digest in "
