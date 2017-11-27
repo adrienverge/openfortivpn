9303a53217dea0dca9cd4b3ca80a914185da098c
Martin Hecht <mrbaseman@gmx.de>
2017-11-27 15:02:23 +0100

    call aclocal from autogen.sh only if it exists
    the automake manual discourages the use of aclocal because on the long
    term it is supposed to go away. However, with automake 1.15 it still
    needs to be called (otherwise even built-in macros are not working).
    With the current commit a smooth transition towards future autotools
    that don't provide aclocal anymore should be possible

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -exu
 
-if ! type aclocal >/dev/null 2>/dev/null ; then
-  echo "aclocal not found - please install automake and autoconf" >&2
-  exit 1
-fi
 if ! type autoconf >/dev/null 2>/dev/null ; then
   echo "autoconf not found - please install it" >&2
   exit 1
 fi
+
 if ! type automake >/dev/null 2>/dev/null ; then
   echo "automake not found - please install it" >&2
   exit 1
 fi
 
-aclocal
+if type aclocal >/dev/null 2>/dev/null ; then
+  aclocal
+fi
+
 autoconf
 automake --add-missing
 
