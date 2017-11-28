4b0c31df67bb7e0f5be5cd93c4612d213fa74a8c
Dimitri Papadopoulos <DimitriPapadopoulos@users.noreply.github.com>
2017-11-28 09:00:56 +0100

    Bourne shell
    * Standard stdout/stderr redirection.
    * The last message is informational, not an error. Use sdtout, not stderr.

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -exu
 
-if ! type autoconf >/dev/null 2>/dev/null ; then
+if ! type autoconf >/dev/null 2>&1 ; then
   echo "autoconf not found - please install it" >&2
   exit 1
 fi
 
-if ! type automake >/dev/null 2>/dev/null ; then
+if ! type automake >/dev/null 2>&1 ; then
   echo "automake not found - please install it" >&2
   exit 1
 fi
 
-if type aclocal >/dev/null 2>/dev/null ; then
+if type aclocal >/dev/null 2>&1 ; then
   aclocal
 fi
 
 autoconf
 automake --add-missing
 
-echo "now you can run ./configure && make to build openfortivpn" >&2
+echo "now you can run ./configure && make to build openfortivpn"
