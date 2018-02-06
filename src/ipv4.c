890f33887e64308fb1cb55e1591340a60939e898
jon2kx <jon2kx@gmail.com>
2018-02-06 17:58:41 -0500

    Update ipv4.c
    line 141 was still to long/did not comply with coding style

--- src/ipv4.c
+++ src/ipv4.c
@@ -136,12 +136,8 @@ static int ipv4_get_route(struct rtentry *route)
 	FILE *fp;
 	FILE *wnst;
 	int len = sizeof(buffer) - 1;
-	char *saveptr3 = NULL;
-	
-	// Open the command for reading and
-	// properly account for the path on
-	// the local linux machine 
-	
+	char *saveptr3 = NULL;	
+	// Open the command for reading
 	wnst = popen("which netstat");
 	fp = popen("wnst -f inet -rn", "r");
 	if (fp == NULL)
