c3d7c671b22e4ad237f7f8e54d65990fb3a4d5d7
Dimitri Papadopoulos <DimitriPapadopoulos@users.noreply.github.com>
2018-05-13 19:11:04 +0200

    Balance directory tree

--- tests/ci/install_openssl.sh
+++ tests/ci/install_openssl.sh
@@ -4,14 +4,14 @@ set -e
 
 PREFIX=$1
 
-[[ -x $PREFIX/bin/openssl ]] && exit 0
+[[ -x ${PREFIX}/bin/openssl ]] && exit 0
 
 VERSION=1.0.2o
 SRC=https://www.openssl.org/source/openssl-${VERSION}.tar.gz
 
 wget -O openssl.tar.gz $SRC
 tar -xf openssl.tar.gz -C $HOME
-cd $HOME/openssl-$VERSION
+cd ${HOME}/openssl-${VERSION}
 ./config --prefix=$PREFIX shared -fPIC
 make
 make install
