89a91e42ab4a0c01bf91ad7b7547eb9a4e642845
Dimitri Papadopoulos <DimitriPapadopoulos@users.noreply.github.com>
2018-05-15 15:36:17 +0200

    Quote shell variables

--- tests/ci/install_astyle.sh
+++ tests/ci/install_astyle.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-PREFIX=$1
+PREFIX="$1"
 
-ln -fs ${PREFIX}/bin/astyle ${HOME}/bin/astyle
-[ -x ${PREFIX}/bin/astyle ] && exit 0
+ln -fs "${PREFIX}/bin/astyle" "${HOME}/bin/astyle"
+[ -x "${PREFIX}/bin/astyle" ] && exit 0
 
 VERSION=3.1
-SRC=https://sourceforge.net/projects/astyle/files/astyle/astyle%20${VERSION}/astyle_${VERSION}_linux.tar.gz/download
+SRC="https://sourceforge.net/projects/astyle/files/astyle/astyle%20${VERSION}/astyle_${VERSION}_linux.tar.gz/download"
 
-wget -O astyle.tar.gz $SRC
-tar -xf astyle.tar.gz -C $HOME
-cd ${HOME}/astyle/build/gcc
+wget -O astyle.tar.gz "$SRC"
+tar -xf astyle.tar.gz -C "$HOME"
+cd "${HOME}/astyle/build/gcc"
 make
-make prefix=$PREFIX install
+make prefix="$PREFIX" install
