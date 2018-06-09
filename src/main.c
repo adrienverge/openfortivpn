adrienverge/openfortivpn

commit bec10e558dd093643785f882e5aacf1742b7ea88

Rephrase --half-internet-routes documentation
Fixes #260.

src/main.c
@@ -63,7 +63,8 @@
 "  --set-routes=[01]             Set if we should configure output roues through\n" \
 "                                the VPN when tunnel is up.\n" \
 "  --no-routes                   Do not configure routes, same as --set-routes=0.\n" \
-"  --half-internet-routes=[01]   Add /1-routes instead of replacing the default route\n" \
+"  --half-internet-routes=[01]   Add two 0.0.0.0/1 and 128.0.0.0/1 routes with higher" \
+"                                priority instead of replacing the default route.\n" \
 "  --set-dns=[01]                Set if we should add VPN name servers in\n" \
 "                                /etc/resolv.conf\n" \
 "  --no-dns                      Do not reconfigure DNS, same as --set-dns=0\n" \

doc/openfortivpn.1.in
@@ -68,8 +68,9 @@ Set if we should try to configure IP routes through the VPN when tunnel is up. I
 
 \fB\-\-no-routes\fR is the same as \fB\-\-set-routes=\fI0\fR.
 .TP
-\fB\-\-half-internet-routes=\fI<bool>\fR, if set to 1, tells openfortivpn not to 
-replace the default route by a different one, but set up two separate /1-routes instead.
+\fB\-\-half-internet-routes=\fI<bool>\fR, if set to 1, tells openfortivpn not
+to replace the default route by a different one, but to set up two 0.0.0.0/1
+and 128.0.0.0/1 routes with higher priority instead.
 .TP
 \fB\-\-set-dns=\fI<bool>\fR, \fB\-\-no-dns\fR
 Set if we should add VPN nameservers in /etc/resolv.conf when tunnel is up. If used multiple times, the last one takes priority.

commit 5cda41844d5c2b9c6a5f3a56506d465306aefe06

Ooops... Fix --help output.
Typo introduced by bec10e5 has been breaking --help output.

src/main.c
@@ -63,7 +63,7 @@
 "  --set-routes=[01]             Set if openfortivpn should configure output roues through\n" \
 "                                the VPN when tunnel is up.\n" \
 "  --no-routes                   Do not configure routes, same as --set-routes=0.\n" \
-"  --half-internet-routes=[01]   Add two 0.0.0.0/1 and 128.0.0.0/1 routes with higher" \
+"  --half-internet-routes=[01]   Add two 0.0.0.0/1 and 128.0.0.0/1 routes with higher\n" \
 "                                priority instead of replacing the default route.\n" \
 "  --set-dns=[01]                Set if openfortivpn should add VPN name servers in\n" \
 "                                /etc/resolv.conf\n" \