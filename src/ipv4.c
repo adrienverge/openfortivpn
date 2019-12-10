c978109349b4d3e2e695546688c9a6ddfbe82113
Martin Hecht <mrbaseman@gmx.de>
2019-12-10 17:35:42 +0100

    implement simpler code in replace_char

--- src/ipv4.c
+++ src/ipv4.c
@@ -1008,11 +1008,11 @@ int ipv4_restore_routes(struct tunnel *tunnel)
 
 static inline char* replace_char(char* str, char find, char replace)
 {
-	char *current_pos = strchr(str,find);
-	while (current_pos) {
-		*current_pos = replace;
-		current_pos = strchr(current_pos,find);
-	}
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+		if (str[i] == find)
+			str[i] = replace;
 	return str;
 }
 
