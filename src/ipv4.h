0952b37012167648abfcb6b9ca5803f2502b3ac6
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2020-05-08 16:08:48 +0200

    These headers are not needed, even on FreeBSD
    <sys/mutex.h>  defines mtx_* functions
    <net/if.h>     defines if_* functions and if_nameindex structure
    <net/if_var.h> ?

--- src/ipv4.h
+++ src/ipv4.h
@@ -19,18 +19,9 @@
 #define OPENFORTIVPN_IPV4_H
 
 #include <sys/types.h>
-#ifdef HAVE_SYS_MUTEX_H
-/* Mac OS X and BSD wants this explicit include */
-#include <sys/mutex.h>
-#endif
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <net/route.h>
-#include <net/if.h>
-#ifdef HAVE_NET_IF_IF_VAR_H
-/* on FreeBSD we need this additional header */
-#include <net/if_var.h>
-#endif
 
 #if !HAVE_RT_ENTRY_WITH_RT_DST
 /*
