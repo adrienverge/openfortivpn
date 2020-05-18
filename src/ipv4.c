adrienverge/openfortivpn

commit c81fde1aa474108ba5c0b74e66b9e91a2a41b5e1

Better document buffer lengths

src/ipv4.c
@@ -1056,8 +1056,10 @@ int ipv4_add_nameservers_to_resolv_conf(struct tunnel *tunnel)
 	int ret = -1;
 	FILE *file;
 	struct stat stat;
-	char ns1[28], ns2[28]; // 11 + 15 + 1 + 1
-	char dns_suffix[MAX_DOMAIN_LENGTH+8];  // 7 + MAX_DOMAIN_LENGTH + 1
+	static const size_t NS_SIZE = ARRAY_SIZE("nameserver xxx.xxx.xxx.xxx");
+	char ns1[NS_SIZE], ns2[NS_SIZE];
+	static const size_t DNS_SUFFIX_SIZE = ARRAY_SIZE("search ") + MAX_DOMAIN_LENGTH;
+	char dns_suffix[DNS_SUFFIX_SIZE];
 	char *buffer = NULL;
 	int use_resolvconf = 0;
 
@@ -1254,8 +1256,10 @@ int ipv4_del_nameservers_from_resolv_conf(struct tunnel *tunnel)
 	int ret = -1;
 	FILE *file;
 	struct stat stat;
-	char ns1[27], ns2[27]; // 11 + 15 + 1
-	char dns_suffix[MAX_DOMAIN_LENGTH+8];  // 7 + MAX_DOMAIN_LENGTH + 1
+	static const size_t NS_SIZE = ARRAY_SIZE("nameserver xxx.xxx.xxx.xxx");
+	char ns1[NS_SIZE], ns2[NS_SIZE];
+	static const size_t DNS_SUFFIX_SIZE = ARRAY_SIZE("search ") + MAX_DOMAIN_LENGTH;
+	char dns_suffix[DNS_SUFFIX_SIZE];
 	char *buffer = NULL;
 	char *saveptr = NULL;
 

src/io.c
@@ -473,7 +473,7 @@ static void *ssl_read(void *arg)
 
 		if (tunnel->state == STATE_CONNECTING) {
 			if (packet_is_ip_plus_dns(packet)) {
-				char line[57]; // 1 + 15 + 7 + 15 + 2 + 15 + 1 + 1
+				char line[ARRAY_SIZE("[xxx.xxx.xxx.xxx], ns [xxx.xxx.xxx.xxx, xxx.xxx.xxx.xxx]")];
 
 				set_tunnel_ips(tunnel, packet);
 				strcpy(line, "[");

tests/lint/line_length.py
@@ -39,7 +39,7 @@ def endswithstring(line):
         True if line ends with string, False otherwise.
 
     """
-    for end in ('"', '",', '");', '";', '" \\'):
+    for end in ('"', '",', '");', '";', '" \\', ')];'):
         if line.endswith(end):
             return True
     return False

commit e840fba3d96949aef8503843dd66cecf4e6e3a75

Fix out of bounds array access
Array size was to short by one byte for the terminal "\n".
This bug has been partly introduced by c81fde1 for arrays ns1 and ns2
but was already present for dns_suffix, which had confused me.

Anyway, this confusion precisely shows that c81fde1 was a good idea!

src/ipv4.c
@@ -1063,10 +1063,10 @@ int ipv4_add_nameservers_to_resolv_conf(struct tunnel *tunnel)
 	int ret = -1;
 	FILE *file;
 	struct stat stat;
-#define NS_SIZE ARRAY_SIZE("nameserver xxx.xxx.xxx.xxx")
+#define NS_SIZE ARRAY_SIZE("nameserver xxx.xxx.xxx.xxx\n")
 	char ns1[NS_SIZE], ns2[NS_SIZE];
 #undef NS_SIZE
-#define DNS_SUFFIX_SIZE (ARRAY_SIZE("search ") + MAX_DOMAIN_LENGTH)
+#define DNS_SUFFIX_SIZE (ARRAY_SIZE("search \n") + MAX_DOMAIN_LENGTH)
 	char dns_suffix[DNS_SUFFIX_SIZE];
 #undef DNS_SUFFIX_SIZE
 	char *buffer = NULL;

tests/lint/checkpatch.sh
@@ -12,7 +12,7 @@ for file in "$@"; do
 
   "$checkpatch_path" --no-tree --terse \
     --max-line-length=90 \
-    --ignore LEADING_SPACE,SPDX_LICENSE_TAG,CODE_INDENT,NAKED_SSCANF,VOLATILE,NEW_TYPEDEFS,LONG_LINE,LONG_LINE_STRING \
+    --ignore LEADING_SPACE,SPDX_LICENSE_TAG,CODE_INDENT,NAKED_SSCANF,VOLATILE,NEW_TYPEDEFS,LONG_LINE,LONG_LINE_STRING,QUOTED_WHITESPACE_BEFORE_NEWLINE \
     -f "$file" | tee "$tmp"
   
   if [ -s "$tmp" ]; then