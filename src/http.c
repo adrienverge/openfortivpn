adrienverge/openfortivpn

commit 35bb04c49b1645828daa2d2b7a5e9094b7d80214

Linux kernel coding style
Running checkpatch.pl with option --subjective catches additional
issues. As the name implies they are subjective, we probably can
disregard most of them.

src/http.c
@@ -50,10 +50,10 @@
 static void url_encode(char *dest, const char *str)
 {
 	while (*str != '\0') {
-		if (isalnum(*str) || *str == '-' || *str == '_'
-		    || *str == '.' || *str == '~')
+		if (isalnum(*str) || *str == '-' || *str == '_' ||
+		    *str == '.' || *str == '~') {
 			*dest++ = *str;
-		else {
+		} else {
 			static const char hex[] = "0123456789ABCDEF";
 
 			*dest++ = '%';
@@ -122,11 +122,8 @@ int http_send(struct tunnel *tunnel, const char *request, ...)
 }
 
 
-static const char *find_header(
-        const char *res,
-        const char *header,
-        uint32_t response_size
-)
+static const char *find_header(const char *res, const char *header,
+                               uint32_t response_size)
 {
 	const char *line = res;
 
@@ -154,11 +151,8 @@ static const char *find_header(
  * @return     1         in case of success
  *             < 0       in case of error
  */
-int http_receive(
-        struct tunnel *tunnel,
-        char **response,
-        uint32_t *response_size
-)
+int http_receive(struct tunnel *tunnel,
+                 char **response, uint32_t *response_size)
 {
 	uint32_t capacity = HTTP_BUFFER_SIZE;
 	char *buffer;
@@ -376,11 +370,7 @@ end:
 }
 
 
-static int get_auth_cookie(
-        struct tunnel *tunnel,
-        char *buf,
-        uint32_t buffer_size
-)
+static int get_auth_cookie(struct tunnel *tunnel, char *buf, uint32_t buffer_size)
 {
 	int ret = 0;
 	const char *line;
@@ -425,12 +415,8 @@ static void delay_otp(struct tunnel *tunnel)
 }
 
 
-static int try_otp_auth(
-        struct tunnel *tunnel,
-        const char *buffer,
-        char **res,
-        uint32_t *response_size
-)
+static int try_otp_auth(struct tunnel *tunnel, const char *buffer,
+                        char **res, uint32_t *response_size)
 {
 	char data[256];
 	char path[40];
@@ -688,9 +674,8 @@ int auth_log_in(struct tunnel *tunnel)
 		         username, realm, reqid, polid, group, tokenresponse);
 
 		delay_otp(tunnel);
-		ret = http_request(
-		              tunnel, "POST", "/remote/logincheck",
-		              data, &res, &response_size);
+		ret = http_request(tunnel, "POST", "/remote/logincheck",
+		                   data, &res, &response_size);
 		if (ret != 1)
 			goto end;
 
@@ -756,8 +741,7 @@ static int parse_xml_config(struct tunnel *tunnel, const char *buffer)
 	val = buffer;
 	while ((val = xml_find('<', "dns", val, 2))) {
 		if (xml_find(' ', "domain=", val, 1)) {
-			tunnel->ipv4.dns_suffix
-			        = xml_get(xml_find(' ', "domain=", val, 1));
+			tunnel->ipv4.dns_suffix = xml_get(xml_find(' ', "domain=", val, 1));
 			log_debug("found dns suffix %s in xml config\n",
 			          tunnel->ipv4.dns_suffix);
 			break;

src/hdlc.c
@@ -132,7 +132,7 @@ ssize_t hdlc_encode(uint8_t *frame, size_t frmsize,
 	if (frmsize < 7)
 		return ERR_HDLC_BUFFER_TOO_SMALL;
 
-	// In theory each frame begins with a Flag Sequence, but it is ommitted
+	// In theory each frame begins with a Flag Sequence, but it is omitted
 	// if the previous frame ends with a Flag Sequence.
 	if (need_flag_sequence)
 		frame[written++] = 0x7e;

src/io.c
@@ -49,9 +49,9 @@ typedef semaphore_t os_semaphore_t;
 
 #define SEM_INIT(sem, x, value)	semaphore_create(mach_task_self(), sem, \
 								SYNC_POLICY_FIFO, value)
-#define SEM_WAIT(sem)			semaphore_wait(*sem)
-#define SEM_POST(sem)			semaphore_signal(*sem)
-#define SEM_DESTROY(sem)		semaphore_destroy(mach_task_self(), *sem)
+#define SEM_WAIT(sem)			semaphore_wait(*(sem))
+#define SEM_POST(sem)			semaphore_signal(*(sem))
+#define SEM_DESTROY(sem)		semaphore_destroy(mach_task_self(), *(sem))
 
 #else
 
@@ -74,14 +74,16 @@ static pthread_mutex_t *lockarray;
 static void lock_callback(int mode, int type, const char *file, int line)
 {
 	if (mode & CRYPTO_LOCK)
-		pthread_mutex_lock(&(lockarray[type]));
+		pthread_mutex_lock(&lockarray[type]);
 	else
-		pthread_mutex_unlock(&(lockarray[type]));
+		pthread_mutex_unlock(&lockarray[type]);
 }
+
 static unsigned long thread_id(void)
 {
 	return (unsigned long) pthread_self();
 }
+
 static void init_ssl_locks(void)
 {
 	int i;
@@ -89,17 +91,18 @@ static void init_ssl_locks(void)
 	lockarray = (pthread_mutex_t *) OPENSSL_malloc(CRYPTO_num_locks() *
 	                sizeof(pthread_mutex_t));
 	for (i = 0; i < CRYPTO_num_locks(); i++)
-		pthread_mutex_init(&(lockarray[i]), NULL);
+		pthread_mutex_init(&lockarray[i], NULL);
 	CRYPTO_set_id_callback((unsigned long (*)()) thread_id);
 	CRYPTO_set_locking_callback((void (*)()) lock_callback);
 }
+
 static void destroy_ssl_locks(void)
 {
 	int i;
 
 	CRYPTO_set_locking_callback(NULL);
 	for (i = 0; i < CRYPTO_num_locks(); i++)
-		pthread_mutex_destroy(&(lockarray[i]));
+		pthread_mutex_destroy(&lockarray[i]);
 	OPENSSL_free(lockarray);
 }
 #else

src/ipv4.c
@@ -220,7 +220,7 @@ cleanup:
 	log_debug("netstat_path: %s\n", netstat_path);
 
 	// Open the command for reading
-	fp = popen(NETSTAT_PATH" -f inet -rn", "r");
+	fp = popen(NETSTAT_PATH " -f inet -rn", "r");
 	if (fp == NULL) {
 		err = ERR_IPV4_SEE_ERRNO;
 		goto end;
@@ -475,7 +475,6 @@ cleanup:
 			       | ((mask & 0xff0000) >> 8)
 			       | ((mask & 0xff00) << 8)
 			       | ((mask & 0xff) << 24);
-
 		}
 		log_debug_details("- Destination IP Hex: %x\n", dest);
 		log_debug_details("- Destination Mask Hex: %x\n", mask);
@@ -846,11 +845,9 @@ int ipv4_add_split_vpn_route(struct tunnel *tunnel, char *dest, char *mask,
 	if ((tunnel->ipv4.split_rt == NULL)
 	    || ((tunnel->ipv4.split_routes % STEP_SPLIT_ROUTES) == 0)) {
 		void *new_ptr
-		        = realloc(
-		                  tunnel->ipv4.split_rt,
+		        = realloc(tunnel->ipv4.split_rt,
 		                  (size_t) (tunnel->ipv4.split_routes + STEP_SPLIT_ROUTES)
-		                  * sizeof(*(tunnel->ipv4.split_rt))
-		          );
+		                  * sizeof(*(tunnel->ipv4.split_rt)));
 		if (new_ptr == NULL)
 			return ERR_IPV4_NO_MEM;
 		tunnel->ipv4.split_rt = new_ptr;
@@ -1001,11 +998,10 @@ int ipv4_set_tunnel_routes(struct tunnel *tunnel)
 	if (tunnel->ipv4.split_routes)
 		// try even if ipv4_protect_tunnel_route has failed
 		return ipv4_set_split_routes(tunnel);
-	else if (ret == 0) {
+	else if (ret == 0)
 		return ipv4_set_default_routes(tunnel);
-	} else {
+	else
 		return ret;
-	}
 }
 
 int ipv4_restore_routes(struct tunnel *tunnel)
@@ -1036,7 +1032,6 @@ int ipv4_restore_routes(struct tunnel *tunnel)
 				log_warn("Could not restore default route (%s). Already restored?\n",
 				         err_ipv4_str(ret));
 			}
-
 		}
 	} else {
 		log_debug("Route to vpn server was not added\n");

src/log.c
@@ -87,6 +87,7 @@ void increase_verbosity(void)
 	if (loglevel < OFV_LOG_DEBUG_ALL)
 		loglevel++;
 }
+
 void decrease_verbosity(void)
 {
 	if (loglevel > OFV_LOG_MUTE)

src/main.c
@@ -111,7 +111,7 @@ PPPD_USAGE \
 "  -h --help                     Show this help message and exit.\n" \
 "  --version                     Show version and exit.\n" \
 "  -c <file>, --config=<file>    Specify a custom config file (default:\n" \
-"                                "SYSCONFDIR"/openfortivpn/config).\n" \
+"                                " SYSCONFDIR "/openfortivpn/config).\n" \
 "  -u <user>, --username=<user>  VPN account username.\n" \
 "  -p <pass>, --password=<pass>  VPN account password.\n" \
 "  -o <otp>, --otp=<otp>         One-Time-Password.\n" \
@@ -170,7 +170,7 @@ PPPD_USAGE \
 "Config file:\n" \
 "  Options can be taken from a configuration file. Options passed in the\n" \
 "  command line will override those from the config file, though. The default\n" \
-"  config file is "SYSCONFDIR"/openfortivpn/config,\n" \
+"  config file is " SYSCONFDIR "/openfortivpn/config,\n" \
 "  but this can be set using the -c option.\n" \
 "  A simple config file example looks like:\n" \
 "      # this is a comment\n" \
@@ -185,7 +185,7 @@ PPPD_USAGE \
 int main(int argc, char **argv)
 {
 	int ret = EXIT_FAILURE;
-	const char *config_file = SYSCONFDIR"/openfortivpn/config";
+	const char *config_file = SYSCONFDIR "/openfortivpn/config";
 	const char *host;
 	char *port_str;
 

src/tunnel.c
@@ -678,7 +678,6 @@ static int tcp_connect(struct tunnel *tunnel)
 					eol = strstr(response, HTTP_EOL[i]);
 				response = eol;
 			}
-
 		}
 
 		free(env_proxy); // release memory allocated by strdup()
@@ -842,9 +841,8 @@ int ssl_connect(struct tunnel *tunnel)
 		log_error("Could not load OS OpenSSL files.\n");
 
 	if (tunnel->config->ca_file) {
-		if (!SSL_CTX_load_verify_locations(
-		            tunnel->ssl_context,
-		            tunnel->config->ca_file, NULL)) {
+		if (!SSL_CTX_load_verify_locations(tunnel->ssl_context,
+		                                   tunnel->config->ca_file, NULL)) {
 			log_error("SSL_CTX_load_verify_locations: %s\n",
 			          ERR_error_string(ERR_peek_last_error(), NULL));
 			return 1;
@@ -919,7 +917,6 @@ int ssl_connect(struct tunnel *tunnel)
 
 	/* Use engine for PIV if user-cert config starts with pkcs11 URI: */
 	if (tunnel->config->use_engine > 0) {
-
 		ENGINE *e;
 
 		ENGINE_load_builtin_engines();
@@ -991,9 +988,9 @@ int ssl_connect(struct tunnel *tunnel)
 		}
 
 		if (tunnel->config->user_key) {
-			if (!SSL_CTX_use_PrivateKey_file(
-			            tunnel->ssl_context, tunnel->config->user_key,
-			            SSL_FILETYPE_PEM)) {
+			if (!SSL_CTX_use_PrivateKey_file(tunnel->ssl_context,
+			                                 tunnel->config->user_key,
+			                                 SSL_FILETYPE_PEM)) {
 				log_error("SSL_CTX_use_PrivateKey_file: %s\n",
 				          ERR_error_string(ERR_peek_last_error(), NULL));
 				return 1;

commit 79f13996021182d461b409bf87d9454302433b4b

Fix regression in coding style
Introduced by 35bb04c because of Travis CI failures.

src/http.c
@@ -741,7 +741,8 @@ static int parse_xml_config(struct tunnel *tunnel, const char *buffer)
 	val = buffer;
 	while ((val = xml_find('<', "dns", val, 2))) {
 		if (xml_find(' ', "domain=", val, 1)) {
-			tunnel->ipv4.dns_suffix = xml_get(xml_find(' ', "domain=", val, 1));
+			tunnel->ipv4.dns_suffix
+			        = xml_get(xml_find(' ', "domain=", val, 1));
 			log_debug("found dns suffix %s in xml config\n",
 			          tunnel->ipv4.dns_suffix);
 			break;