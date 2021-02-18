79bf3d0f41294f88d8ac45a22e1947564cf83e67
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-02-18 11:04:14 +0100

    Travis CI → GitHub Actions

--- tests/ci/install_astyle.sh
+++ tests/ci/install_astyle.sh
@@ -4,7 +4,7 @@ set -e
 
 PREFIX="$1"
 
-ln -fs "${PREFIX}/bin/astyle" "${HOME}/bin/astyle"
+ln -fs "${PREFIX}/bin/astyle" "${HOME}/.local/bin/astyle"
 [ -x "${PREFIX}/bin/astyle" ] && exit 0
 
 VERSION=3.1
