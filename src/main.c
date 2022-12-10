adrienverge/openfortivpn

commit 9b990e7df56dec53a6c5250fa63a5a3458ed82b9

Prepend "SVPNCOOKIE=" to the given cookie if missing (#1035)
Prepend "SVPNCOOKIE=" to the given cookie if missing

It is maybe not obvious that currently the cookie is only accepted
if it has "SVPNCOOKIE=", so add it when it is missing.

Co-authored-by: Dimitri Papadopoulos Orfanos <3234522+DimitriPapadopoulos@users.noreply.github.com>

src/main.c
@@ -186,6 +186,27 @@ PPPD_USAGE \
 "      trusted-cert = othercertificatedigest6631bf...\n" \
 "  For a full-featured configuration see man openfortivpn(1).\n"
 
+/**
+ * Returns the given "input" prefixed with "prefix" in a dynamically
+ * allocated string. This behaves exactly like "strdup" if "input" already
+ * starts with "prefix".
+ */
+static char *strdup_with_prefix(const char *input, const char *prefix)
+{
+	size_t prefix_len = strlen(prefix);
+	char *output;
+
+	if (memcmp(prefix, input, prefix_len) == 0)
+		return strdup(input);
+
+	output = malloc(prefix_len + strlen(input));
+	if (output) {
+		strcpy(output, prefix);
+		strcpy(output + prefix_len, input);
+	}
+	return output;
+}
+
 int main(int argc, char **argv)
 {
 	int ret = EXIT_FAILURE;
@@ -517,7 +538,7 @@ int main(int argc, char **argv)
 			}
 			if (strcmp(long_options[option_index].name,
 			           "cookie") == 0) {
-				cli_cfg.cookie = strdup(optarg);
+				cli_cfg.cookie = strdup_with_prefix(optarg, "SVPNCOOKIE=");
 				break;
 			}
 			if (strcmp(long_options[option_index].name,
@@ -529,7 +550,8 @@ int main(int argc, char **argv)
 					break;
 				}
 				free(cli_cfg.cookie);
-				cli_cfg.cookie = cookie;
+				cli_cfg.cookie = strdup_with_prefix(cookie, "SVPNCOOKIE=");
+				free(cookie);
 				break;
 			}
 			goto user_error;

commit 470eb27ed1c86d2d99f0612813c89b035b62bce8

Fix recent bugs
* Fix the error I introduced in 9b990e7. Sorry about that.
* Fix allocation size, take into account terminating null.

src/main.c
@@ -196,10 +196,10 @@ static char *strdup_with_prefix(const char *input, const char *prefix)
 	size_t prefix_len = strlen(prefix);
 	char *output;
 
-	if (memcmp(prefix, input, prefix_len) == 0)
+	if (strncmp(prefix, input, prefix_len) == 0)
 		return strdup(input);
 
-	output = malloc(prefix_len + strlen(input));
+	output = malloc(prefix_len + strlen(input) + 1);
 	if (output) {
 		strcpy(output, prefix);
 		strcpy(output + prefix_len, input);