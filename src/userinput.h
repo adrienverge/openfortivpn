b52aa8d4941fdf3b2b8159628215da01b239973f
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2023-05-06 10:26:46 +0200

    Variant for the © symbol: (C) → (c)

--- src/userinput.h
+++ src/userinput.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2015 Davíð Steinn Geirsson
+ *  Copyright (c) 2015 Davíð Steinn Geirsson
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
