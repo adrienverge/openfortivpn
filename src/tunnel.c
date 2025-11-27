adrienverge/openfortivpn

commit 912f1af06f598555d7b4451e779cc2e68bd865b7

Remove deprecated option `--plugin`

src/tunnel.c
@@ -345,10 +345,6 @@ static int pppd_run(struct tunnel *tunnel)
 			}
 		}
 		if (tunnel->config->pppd_plugin) {
-			if (ofv_append_varr(&pppd_args, "plugin")) {
-				free(pppd_args.data);
-				return 1;
-			}
 			if (ofv_append_varr(&pppd_args, tunnel->config->pppd_plugin)) {
 				free(pppd_args.data);
 				return 1;

src/main.c
@@ -322,7 +322,6 @@ int main(int argc, char *argv[])
 		{"pppd-ifname",          required_argument, NULL, 0},
 		{"pppd-call",            required_argument, NULL, 0},
 		{"pppd-accept-remote",   optional_argument, NULL, 0},
-		{"plugin",               required_argument, NULL, 0}, // deprecated
 #endif
 #if HAVE_USR_SBIN_PPP
 		{"ppp-system",           required_argument, NULL, 0},
@@ -348,7 +347,7 @@ int main(int argc, char *argv[])
 
 		switch (c) {
 		case 0:
-			/* If this option set a flag, do nothing else now. */
+			/* If this option sets a flag, do nothing else now. */
 			if (long_options[option_index].flag != 0)
 				break;
 			if (strcmp(long_options[option_index].name,
@@ -416,16 +415,6 @@ int main(int argc, char *argv[])
 				} else {
 					cli_cfg.pppd_accept_remote = 1;
 				}
-				break;
-			}
-			// --plugin is deprecated, use --pppd-plugin
-			if (cli_cfg.pppd_plugin == NULL &&
-			    strcmp(long_options[option_index].name,
-			           "plugin") == 0) {
-				log_warn("Option --%s is deprecated, use --pppd-plugin\n",
-				         long_options[option_index].name);
-				free(cli_cfg.pppd_plugin);
-				cli_cfg.pppd_plugin = strdup(optarg);
 				break;
 			}
 #endif

commit 42eb72495ba8cf3d5bfd9effb45cf2cfcbca53d5

Fix support for --pppd-plugin (passing plugin to pppd)
Running pppd with plugin specified requires "plugin <plugin>" syntax.

While removing support for deprecated --plugin option the "plugin" part
was accidentally dropped. This introduced regression: --pppd-plugin
doesn't work anymore.

This change fixes:
NetworkManager[22657]: DEBUG:  Starting IO through the tunnel
NetworkManager[22657]: DEBUG:  pppd_read thread
NetworkManager[22657]: DEBUG:  ssl_read thread
NetworkManager[22657]: DEBUG:  ssl_write thread
NetworkManager[22657]: DEBUG:  if_config thread
NetworkManager[22659]: /usr/sbin/pppd: unrecognized option '/usr/lib64/pppd/2.4.7/nm-fortisslvpn-pppd-plugin.so'
NetworkManager[22659]: pppd version 2.4.7
NetworkManager[22659]: Usage: /usr/sbin/pppd [ options ], where options are:
NetworkManager[22659]:         <device>        Communicate over the named device
NetworkManager[22659]:         <speed>                Set the baud rate to <speed>
NetworkManager[22659]:         <loc>:<rem>        Set the local and/or remote interface IP
NetworkManager[22659]:                         addresses.  Either one may be omitted.
NetworkManager[22659]:         asyncmap <n>        Set the desired async map to hex <n>
NetworkManager[22659]:         auth                Require authentication from peer
NetworkManager[22659]:         connect <p>     Invoke shell command <p> to set up the serial line
NetworkManager[22659]:         crtscts                Use hardware RTS/CTS flow control
NetworkManager[22659]:         defaultroute        Add default route through interface
NetworkManager[22659]:         file <f>        Take options from file <f>
NetworkManager[22659]:         modem                Use modem control lines
NetworkManager[22659]:         mru <n>                Set MRU value to <n> for negotiation
NetworkManager[22659]: See pppd(8) for more options.
pppd[22659]: unrecognized option '/usr/lib64/pppd/2.4.7/nm-fortisslvpn-pppd-plugin.so'

Fixes: 912f1af06f59 ("Remove deprecated option `--plugin`")

src/tunnel.c
@@ -357,6 +357,10 @@ static int pppd_run(struct tunnel *tunnel)
 			}
 		}
 		if (tunnel->config->pppd_plugin) {
+			if (ofv_append_varr(&pppd_args, "plugin")) {
+				free(pppd_args.data);
+				return 1;
+			}
 			if (ofv_append_varr(&pppd_args, tunnel->config->pppd_plugin)) {
 				free(pppd_args.data);
 				return 1;